673a312b18a2c39ec9b61b98bf17c8b7fa13d74e
ChemicalIzu <122799730+ChemicalIzu@users.noreply.github.com>
2023-03-08 11:33:31 +0000

    Add files via upload

---
+++ src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+public class Main {
+    public static void main(String[] args) {
+        byte entero1;
+        int entero2;
+        long entero3;
+        float decimal1;
+        double decimal2;
+        boolean verdad = true;
+        char caracter;
+        String texto;
+        System.out.println(entero1 = 2);
+        System.out.println(entero2 = 10);
+        System.out.println(entero3 = 123456789);
+        System.out.println(decimal1 = 1.9F);
+        System.out.println(decimal2= 5.75d);
+        System.out.println(verdad = 5 < 10);
+        System.out.println(caracter = 'f');
+        System.out.println(texto = "Hola Mundo");
+
+
+    }
+}
